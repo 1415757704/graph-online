ddc028c7ef34c24b238ad1319ec7bdeb9a61f5b4
河马先生 <1415757704@qq.com>
2022-04-25 11:13:18 +0800

    project init

---
+++ src/views/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <a-button type="primary">cacas</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+// import HelloWorld from '@/components/HelloWorld1.vue' // @ is an alias to /src
+import { defineComponent } from 'vue'
+
+const Home = defineComponent({})
+
+export default Home
+</script>
